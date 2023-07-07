d0e36ef5da42d022c11da3168706d2765b0a2f30
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2023-07-07 15:35:42 +0200

    Logic added for geocoding API call #1

--- src/components/search.tsx
+++ src/components/search.tsx
@@ -1,21 +1,9 @@
-'use client'
 
-import { useState } from 'react'
-
-
-import { useForm } from "react-hook-form"
+import { useEffect, useState } from "react"
+import axios from "axios";
+import {useRef} from 'react';
 import { zodResolver } from "@hookform/resolvers/zod"
 import * as z from "zod"
- 
-import {
-  Form,
-  FormControl,
-  FormDescription,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
 
 // import { useLocation } from './hooks/useLocation'
 
@@ -28,48 +16,69 @@ import { Dialog,
     DialogFooter,
     DialogHeader,
     DialogTitle,
-    DialogTrigger, } from "./ui/dialog";
-
-    const formSchema = z.object({
-      city: z.string().min(2, {
-        message: "City Name must at least be two letters",
-      }),
-    })    
+    DialogTrigger, } from "./ui/dialog";  
 
 type SearchProps = {
 
 } 
 
+const apiKey = import.meta.env.VITE_API_KEY
+
 export function Search() {
-    
+  const [lat, setLat] = useState(null);
+  const [lng, setLng] = useState(null);
+
+  const [cityName, setCityName] = useState('');
+
+  const handleSubmit = event => {
+    console.log('handleSubmit ran');
+    event.preventDefault(); // 👈️ prevent page refresh
+
+    // 👇️ access input values here
+    console.log(cityName);
+
+    setCityName(cityName);
+  };
+  
+  useEffect(() => {
+    axios.get(`http://api.openweathermap.org/geo/1.0/direct?q=${cityName}&limit=5&appid=${apiKey}`).then((data) => {
+      console.log(data);
+      setCityName(data?.data);
+    });
+  }, []);
+
     return (
       <Dialog>
         <DialogTrigger asChild>
           <Button variant="secondary">
             Search</Button>
         </DialogTrigger>
-        <DialogContent className="sm:max-w-[425px]">
+
+        <DialogContent>
           <DialogHeader>
             <DialogTitle>Search for my City</DialogTitle>
             <DialogDescription>
-              Type in your location and click on the "Search City" button
+              Type in the name of your city and click on the "Search City" button.
             </DialogDescription>
           </DialogHeader>
+          <form onSubmit={handleSubmit}>
           <div className="grid gap-4 py-4">
-            <div className="relative grid items-center grid-cols-4 gap-4">
+             <div className="relative grid items-center grid-cols-4 gap-4">
                 <div className="absolute left-3">
                 <Icons.search stroke="#a09fb1"/>
                 </div>
-              <Input id="name" value="London" placeholder="Search location..." className="col-span-3" />
+              <Input id="cityName" name="cityName" type="text" placeholder="ex. London" onChange={event => setCityName(event.target.value)} value={cityName}
+             className="col-span-3" />
             </div>
           </div> 
-          <DialogFooter>
-            <Button type="submit">Search City</Button>
-          </DialogFooter>
+          <div>
+          <Button type="submit">Search</Button>
+        </div>
+        </form>
         </DialogContent>
       </Dialog>
     )
-  }
+    }
 
 {/* React form hook instead of input and button
 
