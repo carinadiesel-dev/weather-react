57babea1c55e9516116a71c693e2f151d4ec478f
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2023-08-16 09:47:20 +0200

    Wind Speed Working

--- src/components/wind.tsx
+++ src/components/wind.tsx
@@ -1,15 +1,23 @@
+// TODO
+// Wind degree / Direction
+
 import { Icons } from "./icons"
+import { useWeatherContext } from "@/context/WeatherContext";
 
 export const Wind = () => {
+  const {weatherNow} = useWeatherContext();
+  const wind = weatherNow.wind;
+  // const { speed, deg, gust } = wind;
+
   return (
     <div className="grid place-items-center bg-mediumBlue">
       <h3 className="py-5 text-xl">Wind Status</h3>
-
+      <div></div>
       <div className="flex items-center py-3">
         {/* Wind Speed */}
-        <span className="font-bold text-8xl">7</span>
+        <span className="font-bold text-8xl">{wind ? wind.speed : "--"}</span>
         {/* Wind Unit */}
-        <span className="text-2xl font-medium">mph</span>
+        <span className="text-2xl font-medium">meter/s</span>
       </div>
 
       <div className="flex items-center gap-2 py-3">
