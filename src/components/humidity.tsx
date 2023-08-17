daaa6a44a1f10f4ed51aa861b02722604c0293e0
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2023-08-17 09:57:34 +0200

    Humidity Widget working

--- src/components/humidity.tsx
+++ src/components/humidity.tsx
@@ -1,6 +1,12 @@
 import { Progress } from "./ui/progress"
+import { useWeatherContext } from "@/context/WeatherContext";
 
 export const Humidity = () => {
+  const {weatherNow} = useWeatherContext();
+  const main = weatherNow.main;
+  const percentage = (main ? main.humidity : 0)
+
+
   return (
     <div className=" bg-mediumBlue">
         <div className="grid place-items-center">
@@ -8,13 +14,13 @@ export const Humidity = () => {
 
     {/* Humidity in % */}
       <div className="flex items-center py-3">
-        <span className="font-bold text-8xl">84</span>
+        <span className="font-bold text-8xl">{main ? main.humidity : "--"}</span>
         <span className="text-2xl font-medium">%</span>
       </div>
       </div>
     {/* Progress Bar */}
       <div className="py-3 mx-10">
-        <Progress value={84} />
+        <Progress value={percentage} />
       </div>
     </div>
   )
