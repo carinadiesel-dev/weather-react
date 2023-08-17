b04f3017497dc4d2fc2d6c18dc92669d168ed3b4
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2023-08-17 10:00:17 +0200

    Air pressure Widget working

--- src/components/airPressure.tsx
+++ src/components/airPressure.tsx
@@ -1,10 +1,14 @@
+import { useWeatherContext } from "@/context/WeatherContext";
+
 export const AirPressure = () => {
+    const {weatherNow} = useWeatherContext();
+    const main = weatherNow.main;
     return (
         <div className="grid bg-mediumBlue place-items-center">
             <h3 className="pt-5 pb-2 text-xl">Air Pressure</h3>
             <div className="flex items-center justify-center gap-4 py-3">
-                <span className="font-bold text-8xl">998</span>
-                <span className="text-2xl font-medium">mb</span>
+                <span className="font-bold text-8xl">{main ? main.pressure : "--"}</span>
+                <span className="text-2xl font-medium">hPa</span>
             </div>
         </div>
     )
